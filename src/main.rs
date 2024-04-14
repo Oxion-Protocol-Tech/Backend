9e4cc4cf694f399064b473e8900a9fc85d34c5f7
DMraise <maximusnescafe@mail.ru>
2024-04-14 18:25:07 +0300

    v1.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+async fn new_page() -> impl Responder {
+    // Открытие файла и чтение его в строку
+    let content = include_str!("new.html");
+    HttpResponse::Ok().body(content)
+}
+
+
+async fn main_page() -> impl Responder {
+    // Открытие файла и чтение его в строку
+    let content = include_str!("main.html");
+    HttpResponse::Ok().body(content)
+}
+
+
+async fn pools_page() -> impl Responder {
+    // Открытие файла и чтение его в строку
+    let content = include_str!("pools.html");
+    HttpResponse::Ok().body(content)
+}
+
+
+async fn protocol_page() -> impl Responder {
+    // Открытие файла и чтение его в строку
+    let content = include_str!("protocol.html");
+    HttpResponse::Ok().body(content)
+}
+
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/new", web::get().to(new_page)) // Добавление обработчика для /new
+            .route("/pools", web::get().to(pools_page))
+            .route("/main", web::get().to(main_page))
+            .route("/protocol", web::get().to(protocol_page))
+        })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
